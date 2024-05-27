2db00886791a859a7dd66e7efac9c2a04c7a7a03
Pramod <86370487+Pramod3245@users.noreply.github.com>
2024-05-27 12:36:56 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  // your config options here
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
