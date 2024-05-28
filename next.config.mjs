64abc5062f858db1131e764bc6b40a3dee1d8fc2
Pramod <pramod97404@gmail.com>
2024-05-28 14:48:30 +0530

    last changes

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@
 const nextConfig = {
   // Injecting the property "output" with value "export"
   output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
   // other config options here
 }
 
