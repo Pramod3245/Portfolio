c52e952cf4f0af932722fa1d249a30dbf8ed9a28
Pramod <pramod97404@gmail.com>
2024-05-28 15:04:31 +0530

    revert mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
-// next.config.js
-module.exports = {
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  // Injecting the property "output" with value "export"
   output: 'export',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
-};
+  // other config options here
+}
+
+export default (phase, { defaultConfig }) => {
+  return {
+    ...defaultConfig,
+    ...nextConfig
+  }
+}
