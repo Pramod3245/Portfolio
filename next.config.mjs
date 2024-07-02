afa723e92a736c241f94e0476c70f5c130830e7f
Pramod <pramod97404@gmail.com>
2024-07-02 19:37:37 +0530

    commit again

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
-// @ts-check
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  // Injecting the property "output" with value "export"
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
-  // other config options here
-}
-
-export default (phase, { defaultConfig }) => {
-  return {
-    ...defaultConfig,
-    ...nextConfig
-  }
-}
+export default nextConfig;
