78021591e8a462bb3d4f297d5895ea7362094e09
Pramod <pramod97404@gmail.com>
2024-07-05 18:01:41 +0530

    contact me

--- src/app/HeroSection.js
+++ src/app/HeroSection.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { FlipWords } from "../components/ui/flip-words";  // Ensure correct import path for FlipWords
+import { FlipWords } from "../components/ui/flip-words";  
 
 function HeroSection() {
   const words = [
@@ -14,10 +14,20 @@ function HeroSection() {
     "tech enthusiast",
     "results-driven"
   ];
-  
 
   return (
-    <div className="h-[40rem] flex justify-center items-center px-4">
+    <div className="relative h-[40rem] flex justify-center items-center px-4">
+      <div className="absolute top-4 right-4">
+        <a 
+          href="https://www.linkedin.com/in/pramod1malagi" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="text-purple-500 hover:scale-110 transition-transform duration-200"
+          style={{ fontFamily: 'Arial, sans-serif' }}
+        >
+          Get in touch with me
+        </a>
+      </div>
       <div className="text-4xl mx-auto font-normal text-white text-center">
         Welcome to My Portfolio! <br />
         I'm Pramod L Malagi, I am
